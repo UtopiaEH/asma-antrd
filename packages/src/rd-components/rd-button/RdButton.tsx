a6ce767ee6711a7e2616f7e894287573951bdadc
Andrei Grini <Andrei.Grini@avanssoma.no>
2022-10-05 07:52:11 +0000

    Merged PR 1918: side effects

--- packages/src/rd-components/rd-button/RdButton.tsx
+++ packages/src/rd-components/rd-button/RdButton.tsx
@@ -1,11 +1,9 @@
 import { Button, ButtonProps } from 'antd'
 
-const RdButton: React.FC<ButtonProps> = (props) => {
+export const RdButton: React.FC<ButtonProps> = (props) => {
     return (
         <Button {...props} className={`rd-button-wrapper ${props.className ?? ''}`}>
             {props.children}
         </Button>
     )
 }
-
-export default RdButton
