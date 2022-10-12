a79b59f832ac9e5b4ed85234096263d377760998
Grini Andrei <Andrei.Grini@carasent.com>
2022-10-12 16:24:56 +0300

    0.0.43

--- test-site/src/App.tsx
+++ test-site/src/App.tsx
@@ -1,38 +1,21 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import "./App.css";
-import { Deprecated } from "asma-antrd";
-
-const CheckBoxCustom = Deprecated.CheckBoxCustom;
+import { useState } from 'react'
+import './styles/index.scss'
+import { RdButton } from 'asma-antrd'
+import { Topbar } from './components/layout-components/topbar/Topbar'
+import { Sidebar } from './components/layout-components/sidebar/Sidebar'
 
 function App() {
-  const [count, setCount] = useState(0);
+    const [count, setCount] = useState(0)
 
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      <CheckBoxCustom title="Something" />
-    </div>
-  );
+    return (
+        <div className="App">
+            <Topbar />
+            <div className="flex w-full h-[calc(100%-var(--top-bar-height))]">
+                <Sidebar />
+                <div className="flex flex-auto bg-gray-100 w-full h-full overflow-auto"></div>
+            </div>
+        </div>
+    )
 }
 
-export default App;
+export default App
