e69b3061997e4e25e64fb0e0410397060eedac20
Grini Andrei <Andrei.Grini@carasent.com>
2022-10-12 15:04:53 +0300

    antdrd button on styled-components

--- packages/src/rd-components/rd-button/RdButton.tsx
+++ packages/src/rd-components/rd-button/RdButton.tsx
@@ -1,11 +1,71 @@
-import './RdButton.scss'
-
 import { Button, ButtonProps } from 'antd'
+import styled from 'styled-components'
 
-export const RdButton: React.FC<ButtonProps> = (props) => {
+const ButtonTemplate: React.FC<ButtonProps> = (props) => {
     return (
-        <Button {...props} className={`rd-button-wrapper ${props.className ?? ''}`}>
+        <Button {...props} className={`${props.className ?? ''}`}>
             {props.children}
         </Button>
     )
 }
+
+export const RdButton = styled(ButtonTemplate)`
+    border-radius: 3px;
+    border: transparent;
+    color: black;
+    box-shadow: 2px 2px 8px rgba(0, 0, 0, 0.2);
+    transition: all 300ms ease-in-out;
+
+    &.ant-btn-primary {
+        color: var(--colors-text-component);
+        background: var(--colors-gama-500);
+
+        &:hover {
+            color: var(--colors-text-component);
+            background: var(--colors-gama-600);
+        }
+        &:focus {
+            background: var(--colors-gama-500);
+            border: var(--colors-gama-700);
+        }
+        &:active {
+            background: var(--colors-gama-600);
+        }
+    }
+
+    &.ant-btn-default {
+        border: 1px solid var(--colors-gama-500);
+
+        &:hover {
+            color: var(--colors-text-component);
+            background: var(--colors-gama-600);
+            border: var(--colors-gama-600);
+        }
+        &:focus {
+            color: var(--colors-text-component);
+        }
+        &:active {
+            color: var(--colors-text-component);
+            background: var(--colors-gama-600);
+            border: var(--colors-gama-600);
+        }
+    }
+
+    &:disabled,
+    &.ant-btn-default:disabled,
+    &.ant-btn-primary:disabled {
+        cursor: default;
+        border: 1px solid transparent;
+        background: #e5e7eb;
+        color: var(--colors-delta-250);
+
+        &:hover {
+            background: #e5e7eb;
+            color: var(--colors-delta-250);
+        }
+        &:focus {
+            background: #e5e7eb;
+            color: var(--colors-delta-250);
+        }
+    }
+`
