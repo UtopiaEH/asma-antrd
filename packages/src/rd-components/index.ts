d80472085847bc057177a80b5247e1ba92877fcd
Grini Andrei <Andrei.Grini@carasent.com>
2022-10-26 21:04:04 +0300

    0.0.51

--- packages/src/rd-components/index.ts
+++ packages/src/rd-components/index.ts
@@ -1,3 +1,4 @@
 export * from './inputs/rd-input'
+export * from './modals/'
 export * from './rd-button'
 export * from './selects/rd-select'
--- packages/tailwind.config.cjs
+++ packages/tailwind.config.cjs
@@ -13,24 +13,23 @@ module.exports = {
             colors: {
                 'color-inherit': 'inherit',
                 'textcolor-default': 'var(--colors-text-default)',
-                // color themming variables
-                // fretex-slate
+                // alpha
                 'alpha-100': 'var(--colors-alpha-100)',
                 'alpha-500': 'var(--colors-alpha-500)',
                 'alpha-600': 'var(--colors-alpha-600)',
                 'alpha-700': 'var(--colors-alpha-700)',
-                // fretex-red
+                // beta
                 'beta-100': 'var(--colors-beta-100)',
                 'beta-500': 'var(--colors-beta-500)',
                 'beta-700': 'var(--colors-beta-700)',
-                // fretex-green
+                // gama
                 'gama-100': 'var(--colors-gama-100)',
                 'gama-200': 'var(--colors-gama-200)',
                 'gama-300': 'var(--colors-gama-300)',
-                'gama-500': 'var(--colors-gama-500)', // default #1E7F4D //fretex #ADC6BC
-                'gama-600': 'var(--colors-gama-600)', // default #1b7245 //fretex #9BB1A8
+                'gama-500': 'var(--colors-gama-500)',
+                'gama-600': 'var(--colors-gama-600)',
                 'gama-700': 'var(--colors-gama-700)',
-                // fretex-gray
+                // delta
                 'delta-100': 'var(--colors-delta-100)',
                 'delta-200': 'var(--colors-delta-200)',
                 'delta-250': 'var(--colors-delta-250)',
@@ -41,8 +40,25 @@ module.exports = {
                 'delta-900': 'var(--colors-delta-900)',
                 'delta-9000': 'var(--colors-gray-9000)',
                 //
+                'sigma-700': 'var(--colors-sigma-700)',
+                //
                 'textcolor-component': 'var(--colors-text-component)',
                 inherit: 'inherit',
+
+                // buttons
+                'btn-bg': 'var(--colors-btn-bg-standart)',
+                'btn-bg-active': 'var(--colors-btn-bg-active)',
+                'btn-bg-hover': 'var(--colors-btn-bg-hover)',
+                'btn-bg-disabled': 'var(--colors-btn-bg-disabled)',
+                'btn-bg-danger': 'var(--colors-btn-bg-danger)',
+                'btn-bg-danger-active': 'var(--colors-btn-bg-danger-active)',
+                // mini action buttons
+                'btn-bg-mini-action': 'var(--colors-btn-bg-mini-action)',
+                'btn-bg-mini-action-hover': 'var(--colors-btn-bg-mini-action-hover)',
+                //badge
+                'badge-bg': 'var(--colors-badge-bg)',
+                //
+                'color-active-component': 'var(--colors-active-component)',
                 //  // // // //
 
                 //  // // // // // //
