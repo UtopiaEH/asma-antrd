313aaacad43c99f0e7de99f20799a3fcd2d4c5ba
Grini Andrei <Andrei.Grini@carasent.com>
2022-10-28 13:32:17 +0300

    rd button fixed name

--- packages/src/rd-components/rd-button/RdButton.tsx
+++ packages/src/rd-components/rd-button/RdButton.tsx
@@ -2,7 +2,7 @@ import './RdButton.scss'
 
 import { Button, ButtonProps } from 'antd'
 
-export const RdButtonx: React.FC<ButtonProps> = (props) => {
+export const RdButton: React.FC<ButtonProps> = (props) => {
     return (
         <Button {...props} className={`rd-button ${props.className ?? ''}`}>
             {props.children}
