1be839dbd8569d47b4071c8c6d380ae11460b566
Grini Andrei <Andrei.Grini@carasent.com>
2022-10-28 13:23:39 +0300

    v0.0.60

--- packages/src/rd-components/rd-button/RdButton.tsx
+++ packages/src/rd-components/rd-button/RdButton.tsx
@@ -1,73 +1,11 @@
-import { Button, ButtonProps } from 'antd'
-import styled from 'styled-components'
+import './RdButton.scss'
 
-const ButtonTemplate: React.FC<ButtonProps> = (props) => {
+import { Button, ButtonProps } from 'antd'
+
+export const RdButtonx: React.FC<ButtonProps> = (props) => {
     return (
-        <Button {...props} className={`${props.className ?? ''}`}>
+        <Button {...props} className={`rd-button ${props.className ?? ''}`}>
             {props.children}
         </Button>
     )
 }
-
-export const RdButton = styled(ButtonTemplate)`
-    border-radius: 3px;
-    border: 1px solid transparent;
-    color: black;
-    box-shadow: 2px 2px 8px rgba(0, 0, 0, 0.2);
-    transition: all 300ms ease-in-out;
-
-    &.ant-btn-primary {
-        color: var(--colors-text-component);
-        background: var(--colors-gama-500);
-
-        &:hover {
-            color: var(--colors-text-component);
-            background: var(--colors-gama-600);
-        }
-        &:focus {
-            background: var(--colors-gama-500);
-            border: 1px solid var(--colors-gama-700);
-        }
-        &:active {
-            background: var(--colors-gama-600);
-        }
-    }
-
-    &.ant-btn-default {
-        border: 1px solid var(--colors-gama-500);
-
-        &:hover {
-            color: var(--colors-text-component);
-            background: var(--colors-gama-600);
-            border: 1px solid var(--colors-gama-600);
-        }
-        &:focus {
-            color: var(--colors-text-component);
-            background: var(--colors-gama-600);
-            border: 1px solid var(--colors-gama-600);
-        }
-        &:active {
-            color: var(--colors-text-component);
-            background: var(--colors-gama-600);
-            border: 1px solid var(--colors-gama-600);
-        }
-    }
-
-    &:disabled,
-    &.ant-btn-default:disabled,
-    &.ant-btn-primary:disabled {
-        cursor: default;
-        border: 1px solid transparent;
-        background: #e5e7eb;
-        color: var(--colors-delta-250);
-
-        &:hover {
-            background: #e5e7eb;
-            color: var(--colors-delta-250);
-        }
-        &:focus {
-            background: #e5e7eb;
-            color: var(--colors-delta-250);
-        }
-    }
-`
