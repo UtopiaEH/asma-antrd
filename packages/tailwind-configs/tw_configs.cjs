2cd06e70109fa543dc7c7950ada21f9616d28c60
Grini Andrei <Andrei.Grini@carasent.com>
2022-10-29 20:34:35 +0300

    v0.0.66

--- packages/tailwind-configs/tw_configs.cjs
+++ packages/tailwind-configs/tw_configs.cjs
@@ -1,9 +1,11 @@
+const twVariables = require('./tw-variables/tw_variables.cjs')
 const twColors = require('./tw_colors.cjs')
 const twFontFamily = require('./tw_fonts.cjs')
 const twBoxShadow = require('./tw_box_shadow.cjs')
 const twAnimation = require('./tw_animation.cjs')
 
 module.exports = {
+    variables: twVariables,
     colors: twColors,
     fontFamily: twFontFamily,
     boxShadow: twBoxShadow,
