675eed998086bb9dbcdc80c375f9bb3f1dab1d5e
Grini Andrei <Andrei.Grini@carasent.com>
2022-10-29 19:09:09 +0300

    new-box-shadow for tailwind

--- packages/tailwind-configs/tw_box_shadow.cjs
+++ packages/tailwind-configs/tw_box_shadow.cjs
@@ -2,6 +2,7 @@ module.exports = {
     blue: '0px 0px 5px #1890ff',
     red: '0px 0px 5px red',
     ash: '-1px 8px 18px -20px black',
+    sh50: '0px 1px 3px rgba(0, 0, 0, 0.25)',
     sh100: '2px 2px 8px rgba(0, 0, 0, 0.2)',
     cardbox: '0 2px 4px 0 rgba(0, 0, 0, 0.12)',
     'cardbox--2': '0 2px 4px 0 rgba(0, 0, 0, 0.1)',
